af9a921e05dc4811d11e8c158f41422459200c90
Djavani <djavanirgomes@gmail.com>
2017-10-31 22:16:55 -0200

    Commit Inicial

---
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css']
+})
+export class CursoDetalheComponent implements OnInit {
+
+  id: string;
+  inscricao: Subscription;
+
+  constructor(private route: ActivatedRoute) { 
+
+  }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+}
